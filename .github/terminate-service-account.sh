d946ed949c720e9c03b13e4ac0b57646436d1bb3
Melanija Cvetic <melanija.cvetic@mongodb.com>
2025-09-05 11:21:19 +0100

    adds terminate sa

--- .github/terminate-service-account.sh
+++ .github/terminate-service-account.sh
@@ -26,25 +26,24 @@ if [ -z "$MONGODB_ATLAS_ORG_ID" ]; then
   echo "MONGODB_ATLAS_ORG_ID env var is not set"
   exit 1
 fi
+if [ -z "$CLIENT_ID" ]; then
+  echo "CLIENT_ID env var is not set"
+  exit 1
+fi
 
 output=$(
     curl --user "${MONGODB_ATLAS_PUBLIC_API_KEY}:${MONGODB_ATLAS_PRIVATE_API_KEY}" \
     --digest \
     --header "Accept: application/vnd.atlas.2025-03-12+json" \
     --header "Content-Type: application/json" \
-    -X POST "https://cloud.mongodb.com/api/atlas/v2/orgs/${MONGODB_ATLAS_ORG_ID}/serviceAccounts" \
-    -d '{
-    "description": "test service account for atlascli github actions",
-    "name": "atlascli-github-actions-service-account",
-    "roles": [
-        "ORG_OWNER"
-    ],
-    "secretExpiresAfterHours": 8
-    }'
+    -X DELETE "https://cloud.mongodb.com/api/atlas/v2/orgs/${MONGODB_ATLAS_ORG_ID}/serviceAccounts/${CLIENT_ID}"
 )
+error_code=$(echo "$output" | jq -r '.error')
 
-client_id=$(echo "$output" | jq -r '.clientId')
-client_secret=$(echo "$output" | jq -r '.secrets[0].secret')
-
-echo "client_id=$client_id" >> $GITHUB_OUTPUT
-echo "client_secret=$client_secret" >> $GITHUB_OUTPUT
+if [ "$error_code" -ge 300 ]; then
+  echo "Failed to delete service account with Client ID $CLIENT_ID. Response:"
+  echo "$output"
+  exit 1
+else
+  echo "Service account with Client ID $CLIENT_ID has been successfully deleted."
+fi
